b25c5f05e6201ed40e5ca9b95bb6f46f8e69c1b5
Vincent Crooks <crooksv@usf.edu>
2025-10-05 21:55:59 -0400

    base class employee

---
+++ cc_6.js
@@ -0,0 +1,15 @@
+// Coding Challenge 06
+// Vincent Crooks
+
+class Employee {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department
+  }
+
+  describe() {
+    return `Employee: ${this.name} (Dept: ${this.department})`
+  }
+}
+//base class Employee
+
